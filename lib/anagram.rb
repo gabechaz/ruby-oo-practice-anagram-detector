8feba8b8e4550a2b41cc1b3ade846ca1fab8183f
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-21 02:14:56 -0500

    commit tree 55a3881e637d751b713be898d5990631af636274

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(warray)
+        
+        comp1 = self.word.split("").sort
+        warray.select do |wordel|
+            wordel.split("").sort == comp1
+            # binding.pry
+        end
+        
+    end
+end
